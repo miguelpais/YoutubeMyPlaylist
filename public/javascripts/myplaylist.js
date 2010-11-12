2fa1b70ad1276256297d331c66a7a4ff0658ae6d
brunofranco <me.brunoo@gmail.com>
2010-11-12 16:58:26 +0000

    =javascript file

---
+++ public/javascripts/myplaylist.js
@@ -0,0 +1,22 @@
+
+function calcResult(vars) {
+
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	  }
+	else {// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	  }
+	
+	xmlhttp.onreadystatechange=function() {
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			//puts the result in the HTML page
+			putGraph(xmlhttp.responseText);
+		}
+	  }
+	xmlhttp.open("GET","" + vars,true);
+	xmlhttp.send();
+    waiting();
+}
+
+
