6a50ab927044a31d9fda6caa245ef890f29df564
Miguel Pais <miguelpais@gmail.com>
2010-11-12 16:19:11 +0000

    first commit, skeleton rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoutubeMyPlaylist::Application.config.secret_token = 'e986cbe5a942933444b165ea250ac7441cc4634147cf13ac59176753e09389ce47fb37cbf1f9f74f79783f0691dd32260a041444942698ba1a0d28cfa9689cb0'
