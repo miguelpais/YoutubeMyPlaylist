45f644390fb80c83e7c466fad6e7018c699cf363
brunofranco <me.brunoo@gmail.com>
2010-11-13 00:12:31 +0000

    =myplaylist.js

--- public/javascripts/myplaylist.js
+++ public/javascripts/myplaylist.js
@@ -1,22 +1,58 @@
 
-function calcResult(vars) {
+var array = new Array();
 
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	  }
-	else {// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	  }
+function parseVideos() {
 	
-	xmlhttp.onreadystatechange=function() {
-	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			//puts the result in the HTML page
-			putGraph(xmlhttp.responseText);
-		}
-	  }
-	xmlhttp.open("GET","" + vars,true);
-	xmlhttp.send();
-    waiting();
+	var input = document.getElementById("input").value;
+	
+	var videos = input.split("\n");
+	var vid;
+	//~ for (vid in videos) {
+		//~ if(videos[vid]!="")
+			requestVideo(videos);
+	//~ }
 }
 
+function requestVideo(search) {
 
+	var functionBack = "show";
+	var url = "";
+	var i;
+	
+	for(i in search) {
+		url = "http://gdata.youtube.com/feeds/api/videos?q="+search[i]+"&alt=json-in-script&format=5&callback="+functionBack;
+		load(url);
+	}
+	
+	document.getElementById("video").innerHTML=array.toString();
+	
+	$.post("playlist/create", array);
+	
+}
+
+function load(src) {
+	var scripts = document.getElementsByTagName('script');  
+    
+	// prevent from loading same script twice  
+	var alreadyLoaded = false;  
+	for (var i = 0, script; script = scripts[i]; i++) {  
+		if (script.src == src) {  
+			alreadyLoaded = true;  
+			break;  
+		}  
+	}
+    
+	if (!alreadyLoaded) {  
+		var scriptElement = document.createElement('script');
+		scriptElement.setAttribute('type', 'text/javascript');  
+		scriptElement.setAttribute('src', src);  
+		document.documentElement.firstChild.appendChild(scriptElement);  
+	}
+}
+
+function show(data) {
+	
+	url=data.feed.entry[0].id.$t;
+	param = url.split("/");
+	array.push(param[param.length-1]);
+}
