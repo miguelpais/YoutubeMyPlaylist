d449577a24b5f239dfda05b475fef7551a5eebb3
Miguel Pais <miguelpais@gmail.com>
2010-11-13 12:22:54 +0000

    corrected to nasty bugs. The tinyurl logic which could make two different users get the same tinyurl for different playlists. And the javascript that got the youtube videos links could swap the order of them

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -14,13 +14,14 @@ class PlaylistController < ApplicationController
       tracks_obj = params[:tracks_obj]
       tracks = JSON.parse(tracks_obj)
       tracks.each_index do |i| 
+        a = Track.new(:url => tracks[i], :tracknum => i)
+        a.save
         if first
-          a = Track.new(:url => tracks[i], :tracknum => i)
-          tinyurl = Track.count.to_s(26)
+          tinyurl = a.id.to_s(26)
           a.tinyurl = tinyurl
           first = false
         else
-          a = Track.new(:url => tracks[i], :tinyurl => tinyurl, :tracknum => i)
+          a.tinyurl = tinyurl
         end
         a.save
       end
--- public/javascripts/myplaylist.js
+++ public/javascripts/myplaylist.js
@@ -1,6 +1,7 @@
 
 var array = [];
 var videos_to_load = 0;
+var callback_functions = [];
 
 function parseVideos() {	
 	var input = document.getElementById("input").value;
@@ -13,19 +14,34 @@ function parseVideos() {
 
 function requestVideo(search) {
 
-	var functionBack = "show";
 	var url = "";
 	var i;
+	var functionBack = "callback_functions"
 
 	for(i in search) {
 		if (search[i] != "") {
 			videos_to_load += 1;
-		}	
+			var tmp = function() {
+				var index = i;
+				callback_functions[i] = function(data) {
+						url=data.feed.entry[0].id.$t;
+						param = url.split("/");
+						array[index] = param[param.length-1];
+						videos_to_load -= 1;
+
+						if (videos_to_load == 0) {
+							$.post("/playlist/create", {tracks_obj: JSON.stringify(array)}, 
+								function(data) {
+									window.location = "/p/index/" + data;
+								});
+						}
+				};
+			}();	
+		}
 	}
-	
 	for(i in search) {
 		if (search[i] != "") {
-			url = "http://gdata.youtube.com/feeds/api/videos?q="+search[i]+"&alt=json-in-script&format=5&callback="+functionBack;
+			url = "http://gdata.youtube.com/feeds/api/videos?q="+search[i]+"&alt=json-in-script&format=5&callback="+functionBack+"["+i+"]";
 			load(url);
 		}
 	}
@@ -48,19 +64,5 @@ function load(src) {
 		scriptElement.setAttribute('type', 'text/javascript');  
 		scriptElement.setAttribute('src', src);  
 		document.documentElement.firstChild.appendChild(scriptElement);  
-	}
-}
-
-function show(data) {
-	url=data.feed.entry[0].id.$t;
-	param = url.split("/");
-	array.push(param[param.length-1]);
-	videos_to_load -= 1;
-	
-	if (videos_to_load == 0) {
-		$.post("/playlist/create", {tracks_obj: JSON.stringify(array)}, 
-		function(data) {
-			window.location = "/p/index/" + data;
-		});
 	}
 }
